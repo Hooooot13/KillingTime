2f1115bbe9d3c78025cd7f218b03e87473de4d5b
levi.carroll <levi.carroll4.ctr@army.mil>
2023-04-17 13:46:24 -0500

    more from killing time

--- CodingProblemsCSharp/DailyCodingProblems.cs
+++ CodingProblemsCSharp/DailyCodingProblems.cs
@@ -9,10 +9,83 @@ namespace CodingProblemsCSharp
     public class DailyCodingProblems
     {
 
-        //Using the features of C# trivializes this problem.
+        //Given a string of length N reverse the string
         public string reverseWordOrderInString_OneLiner(string S)
         {   
             return string.Join(" ", S.ToLower().Split(' ').Reverse());
+        } //Using the features of C# trivializes this problem.
+        //Since strings are objects and can be seamlessly converted into
+        //  Enumerables wich allow manipulations the whole process 
+        //  is really easy with C#
+        //  I might do this at a later time with another language
+
+
+        //Given an ineger array of N length
+        //  Where each value represents the unit hight of a wall
+        //  Suppose it will rain and all spots between two walls gets filled up
+        //  Compute the number of units enclosed by two walls 
+        //  Do this in O(N) time and O(1) space
+        //  EX1:    GIVEN : [2, 1, 2]           RESULT : 1
+        //          GIVEN : [3, 0, 1, 3, 0, 5]  RESULT : 8
+        //          \_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/
+        //           `````````````````\```````````````````
+        //            `````````````````\`````````````````
+        //           ``````````````````/``````````#```````
+        //            ````/\```````````\````````` #``````
+        //           ````/  \``````````/`````#~~#~#```````
+        //            ```#  #```#~#````\`````#~~#~#``````
+        //           ````####```###````/`````#~##~#```````
+        public int rainTrapped(int[] walls)
+        {
+            int spacesLeft = 0;
+            int spacesRight = 0;
+            int spaces = 0;
+            int potentialSpaces = 0;
+            int localMaxLeft = walls[0];
+            int localMaxLeftIndex = 0;
+            int localMaxRight = walls[^1];
+            int localMaxRightIndex = walls.Length - 1;
+
+
+            for (int i = 0; i <= ((walls.Length - 1) / 2); i++)
+            {
+
+                //if ((walls[i] == localMaxLeft) || (walls[i] == localMaxRight)) continue;
+
+                if (walls[i] > localMaxLeft)
+                {
+                    localMaxLeft = walls[i];
+                    localMaxLeftIndex = i;
+                }
+                else if (walls[i] < localMaxLeft)
+                {
+                    spaces += (localMaxLeft - walls[i]);
+                }
+                else if (walls[walls.Length - 1 - i] > localMaxRight)
+                {
+                    localMaxRight = walls[walls.Length - 1 - i];
+                    localMaxRightIndex = walls.Length - 1 - i;
+                }
+                else if (walls[walls.Length - 1 - i] < localMaxRight)
+                {
+                    spaces += (localMaxRight - walls[i]);
+                }
+
+
+            }
+
+            if ((walls[walls.Length / 2] > localMaxLeft) || (walls[walls.Length / 2] > localMaxRight)) return spaces;
+
+            if (localMaxRight > localMaxLeft)
+            {
+                spaces = spaces - (localMaxRightIndex - (walls.Length / 2) * (localMaxRight - localMaxLeft));
+            }
+            else if (localMaxRight < localMaxLeft)
+            {
+                spaces = spaces - ((walls.Length / 2) - localMaxLeftIndex * (localMaxLeft - localMaxRight));
+            }
+            return spaces;
         }
+
     }
 }
--- CondingProblemsCSharp.UnitTest/DailyCodingProblemsTest.cs
+++ CondingProblemsCSharp.UnitTest/DailyCodingProblemsTest.cs
@@ -19,19 +19,21 @@ namespace CondingProblemsCSharp.UnitTest
         }
 
         [TestCase("Linux is great and you know it ;)", ";) it know you and great is linux")]
-        public void reverseWordOrderInString_GivenString_ReversesWordOrder(string givenString, string revercedString)
-        {
-            string result = _dailyCodingProblems.reverseWordOrderInString(givenString);
-
-            Assert.That(result, Is.EqualTo(revercedString));
-        }
-
-        [TestCase("Linux is great and you know it ;)", ";) it know you and great is linux")]
         public void reverseWordOrderInString_OneLiner_GivenString_ReversesWordOrder(string givenString, string revercedString)
         {
             string result = _dailyCodingProblems.reverseWordOrderInString_OneLiner(givenString);
 
             Assert.That(result, Is.EqualTo(revercedString));
+        }
+
+        [TestCase(new int[] { 2, 1, 2 }, 1)]
+        [TestCase(new int[] { 3, 0, 1, 3, 0, 5 }, 8)]
+        [TestCase(new int[] { 5, 0, 0, 0, 0, 0, 5}, 25)]
+        [TestCase(new int[] { 5, 0, 0, 0, 0, 0, 0 }, 0)]
+        [TestCase(new int[] { 2, 0, 5, 0, 2}, 4)]
+        public void rainTrapped_GivenArray_ReturnsIntOfFreeSpaces(int[] input, int result)
+        {
+            Assert.That(_dailyCodingProblems.rainTrapped(input), Is.EqualTo(result));
         }
     }
 }
